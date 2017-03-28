b88bd40930a4778f20f1cd782e78896af3bb2139
Joe Heitkamp <noetic97@gmail.com>
2017-03-28 17:15:04 -0600

    add vars and card element

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
-var title = document.getElementById('foto-title')
-var cardTitle = document.querySelector('.card-title')
+var titleInput = document.getElementById('foto-title');
+var captionInput = document.getElementById('foto-caption');
+var fileInput = document.getElementById('file');
+var saveButton = document.getElementById('save');
+var cardTitle = document.querySelector('.card-title');
+var cardPhoto = document.querySelector('.card-photo');
+var cardCaption = document.querySelector('.card-caption');
+var trashButton = document.querySelector('.delete');
+var likeButton = document.querySelector('.favorite');
+var card = document.querySelector('.card');
+var newCard = {}
+var cardContainer = document.querySelector('main');
 
-title.addEventListener('input', testText)
+saveButton.addEventListener('click', function () {
+  cardPhoto.src = 'photos/' + fileInput.files[0].name;
+  newCard = new Card(titleInput.value, 'photos/' + fileInput.files[0].name, captionInput.value)
+  addCard(newCard)
+});
 
 
-function testText() {
-  var text = title.value
-  cardTitle.innerText = text
-}
+
+//card constructor
+function Card(title, photo, caption) {
+  this.title = title;
+  this.photo = photo;
+  this.caption = caption;
+  this.favorite = false;
+};
+
+
+
+function addCard(cardNew) {
+  var cardStructure = document.createElement('section')
+  cardStructure.className = "card"
+cardContainer.appendChild(cardStructure, card)
+};
